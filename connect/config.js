72594615caf26a567117807061f63575c1943987
NandoBendungan <88217658+NandoBendungan@users.noreply.github.com>
2022-06-13 16:57:17 +0700

    Add files via upload

---
+++ connect/config.js
@@ -0,0 +1,73 @@
+
+const fs = require("fs")
+const chalk = require("chalk")
+/*Ubah Nama & Dll Cukup Dari sini(Udah Di set biar smua nama & wm Bisa diubah dari sini) 
+Biar Newbie Gapusing ubah nama² 
+Other*/
+global.thum = fs.readFileSync("./worker/media/image/Night.jpg")
+global.log0 = fs.readFileSync("./worker/media/image/Night.jpg")
+global.err4r = fs.readFileSync("./worker/media/image/Night.jpg")
+global.owner = ['6282331243615','6282331243615','6282331243615']
+global.footer = "𝙉𝙞𝙜𝙝𝙩𝘽𝙤𝙩 ~ 𝗡𝗮𝗻𝗱𝗼"
+global.fake = "𝗡𝗶𝗴𝗵𝘁𝗕𝗼𝘁 ~ 𝗡𝗮𝗻𝗱𝗼"
+global.namebot = "𝙽𝚒𝚐𝚑𝚝𝙱𝚘𝚝"
+global.packname = "𝗗𝗢𝗡𝗔𝗦𝗜 𝗩𝗜𝗔 𝗦𝗔𝗪𝗘𝗥𝗜𝗔,𝗗𝗔𝗡𝗔,𝗣𝗨𝗟𝗦𝗔\n𝗗𝗮𝗻𝗮 : 082331243615\n\𝗣𝘂𝗹𝘀𝗮 : 082331243615n\n𝗦𝗮𝘄𝗲𝗿𝗶𝗮 : 𝗛𝘁𝘁𝗽𝘀://𝘀𝗮𝘄𝗲𝗿𝗶𝗮.𝗰𝗼/𝗡𝗶𝗴𝗵𝘁𝗡𝗮𝗻𝗱𝗼\n\n\n\n𝗢𝘄𝗻𝗲𝗿 𝗶𝗻𝗳𝗼\n𝗡𝗮𝗺𝗮 : 𝗡𝗮𝗻𝗱𝗼\n𝗨𝗺𝘂𝗿 : 12 𝘁𝗮𝗵𝘂𝗻\n𝗚𝗮𝗺𝗲 : (𝗦𝘁𝘂𝗺𝗯𝗹𝗲 𝗴𝗮𝘆,𝗠𝗼𝗯𝗶𝗹𝗲 𝗟𝗲𝗴𝗲𝗻𝗱𝘀,𝗖𝗼𝗰,𝗠𝗲𝗻𝗸𝗿𝗲𝗽) 𝗢𝘄𝗻𝗲𝗿 𝗧𝗶𝗱𝗮𝗸 𝗺𝗮𝗶𝗻 𝘆𝗮𝗻𝗴 𝘁𝗶𝗱𝗮𝗸 𝗮𝗱𝗮 𝗽𝗶𝗻𝘁𝘂𝗻𝘆𝗮\n𝗬𝗼𝘂𝘁𝘂𝗯𝗲 : 𝗗𝗨𝗡𝗚𝗔𝗡 𝗔𝗘\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+global.packname2 = "𝗡𝗶𝗴𝗵𝘁𝗕𝗼𝘁 • 𝘀𝘂𝗯𝘀𝗰𝗿𝗶𝗯𝗲 • 𝗗𝘂𝗻𝗴𝗮𝗻 𝗮𝗲 "
+global.author = "𝗡𝗮𝗻𝗱𝗼"
+global.email = "mnandopratama423@gmail.com"
+global.game = "𝗡𝗶𝗴𝗵𝘁𝗕𝗼𝘁 𝗚𝗮𝗺𝗲"
+global.nameown = "©𝗡𝗮𝗻𝗱𝗼"
+global.resultwibu = "Nih kak Resultnya, Dasar Wibu"
+global.nameyt = "𝗗𝗨𝗡𝗚𝗔𝗡 𝗔𝗘"
+global.instagram = "https://instagram.com/heroasta"
+global.sessionName = "session"
+global.github = "https://github.com/NandoBendungan"
+global.done = "𝗗𝗼𝗻𝗲 :')"
+global.linkgrupss = "https://chat.whatsapp.com/C0y1iHxWw9n8odBcK3Rs5z"
+global.antitags = true
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.mess = {
+    success: 'Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    linkm: 'Linknya Mana Kak?',
+    ban: 'Kamu telah dibanned oleh owner, Jika ingin di unbanned chat owner'
+}
+global.BerryAwal = 5000
+global.limitAwal= 20
+global.gamewaktu = 60
+global.waktu = 60000
+
+//-[❌Jangan Ubah bagian ini❌]
+global.tebakgambar = []
+global.rkyt = []
+global.hit = []
+global.banUser = ['6281366679021@s.whatsapp.net']
+global.ntilink = ['120363043965529911@g.us','120363022988111568@g.us']
+global.wlcm = ['120363043965529911@g.us','120363023112413670@g.us','19054091487-1424650263@g.us','120363022988111568@g.us']
+global.gcrevoke = ['19054091487-1424650263@g.us'] 
+global.ntidel = ['120363043965529911@g.us']
+multipref = true;
+oneprefix = false;
+preff = "!"
+//-[❌❌❌❌❌❌❌❌❌❌]
+// REGEXX //
+global.YtIdRegex = /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:|watch\?.*(?:|\&)v=|embed\/|v\/|shorts\/)|youtu\.be\/)([-_0-9A-Za-z]{11}|[-_0-9A-Za-z]{10})/
+global.IgIdRegex = /((?:https?:\/\/)?(?:www\.)?instagram\.com\/(?:p|reel)\/([^/?#&]+))/
+global.FbIdRegex = /(?:https?:\/\/)?(?:www\.)?facebook\.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w\-]*\/)*([\w\-\.]*)/
+global.TwitIdRegex = /(?:http(?:s|):\/\/|)(?:(?:www\.|mobile\.|))(?:twitter\.com)\/([a-zA-Z0-9-_\.]{3,20})\/(?:status)\/([?=0-9a-z]{15,25})([a-zA-Z=0-9]{3,6})/
+global.TtIdRegex = /(?:http(?:s|):\/\/|)(?:www\.|)(?:tiktok.com)\/@([-_0-9A-Za-z\.]{3,20})\/video\/([0-9]{19,25})?.(?:sender_device=pc&sender_web_id=[0-9]{19,25})&.(?:s_from_webapp=v1&is_copy_url=[0-9]{1})|(?:http(?:s|)):\/\/(?:(?:vt.|vm.)tiktok.com)\/(?:[a-z0-9A-Z]{9,15}\/)|(?:http(?:s|)):\/\/(?:t.tiktok.com)\/(?:i18n\/share\/video)\/([&\?\/a-zA-Z0-9=_-]{333,400})/
+
+//𝘿𝙖𝙩𝙖𝙗𝙖𝙨𝙚
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
